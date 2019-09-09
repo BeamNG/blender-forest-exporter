975021686c6ac6ea3cbbf2b9b22530c18630b1a6
Thomas Portassau <tportassau@beamng.com>
2019-09-09 16:02:03 +0200

    port to bpy 2.80

--- io_scene_forest/__init__.py
+++ io_scene_forest/__init__.py
@@ -7,9 +7,9 @@
 
 bl_info = {
     "name": "BeamNG *.forest4.json Exporter",
-    "author": "Dummiesman",
-    "version": (0, 0, 2),
-    "blender": (2, 77, 0),
+    "author": "BeamNG / dmn",
+    "version": (0, 0, 3),
+    "blender": (2, 80, 0),
     "location": "File > Export",
     "description": "Export forest files",
     "warning": "",
@@ -37,18 +37,18 @@ class ExportForest(bpy.types.Operator, ExportHelper):
     bl_label = 'Export Forest v4'
 
     filename_ext = ".forest4.json"
-    filter_glob = StringProperty(
+    filter_glob: StringProperty(
             default="*.forest4.json",
             options={'HIDDEN'},
             )
 
-    forest_item = StringProperty(
+    forest_item: StringProperty(
         name="Mesh name",
         description="This will be the exported mesh in the *.forest4.json file",
         default="",
         )
 
-    selection_only = BoolProperty(
+    selection_only: BoolProperty(
         name="Selection Only",
         description="Export only selected elements",
         default=False,
@@ -72,6 +72,7 @@ class ExportForest(bpy.types.Operator, ExportHelper):
 
         return export_forest.save(self, context, **keywords)
 
+addon_classes = [ExportForest]
 
 # Add to a menu
 def menu_func_export(self, context):
@@ -79,15 +80,17 @@ def menu_func_export(self, context):
 
 
 def register():
-    bpy.utils.register_module(__name__)
+    for c in addon_classes:
+        bpy.utils.register_class(c)
 
-    bpy.types.INFO_MT_file_export.append(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
+    for c in addon_classes:
+        bpy.utils.unregister_class(c)
 
-    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
 if __name__ == "__main__":
     register()
