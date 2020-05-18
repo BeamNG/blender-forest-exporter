fec729c7f734f8c216667eeb8c0a9c2598bac19b
Thomas Portassau <tportassau@beamng.com>
2020-05-18 10:25:35 +0200

    added importer

--- io_scene_forest/__init__.py
+++ io_scene_forest/__init__.py
@@ -6,17 +6,17 @@
 # ##### END LICENSE BLOCK #####
 
 bl_info = {
-    "name": "BeamNG *.forest4.json Exporter",
+    "name": "BeamNG forest item (*.forest4.json)",
     "author": "BeamNG / dmn",
-    "version": (0, 0, 3),
+    "version": (0, 1, 0),
     "blender": (2, 80, 0),
-    "location": "File > Export",
-    "description": "Export forest files",
+    "location": "File > Import-Export",
+    "description": "Import-Export forest files",
     "warning": "",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.7/Py/"
                 "Scripts/Import-Export/forest",
     "support": 'COMMUNITY',
-    "category": "Export"}
+    "category": "Import-Export"}
 
 import bpy
 from bpy.props import (
@@ -30,6 +30,10 @@ from bpy_extras.io_utils import (
         ImportHelper,
         ExportHelper,
         )
+
+from . import import_forest
+from . import export_forest
+
 
 class ExportForest(bpy.types.Operator, ExportHelper):
     """Export to FOREST file format (.FOREST4.JSON)"""
@@ -62,8 +66,6 @@ class ExportForest(bpy.types.Operator, ExportHelper):
         sub.prop(self, "selection_only")
 
     def execute(self, context):
-        from . import export_forest
-
         keywords = self.as_keywords(ignore=("axis_forward",
                                             "axis_up",
                                             "filter_glob",
@@ -72,18 +74,47 @@ class ExportForest(bpy.types.Operator, ExportHelper):
 
         return export_forest.save(self, context, **keywords)
 
-addon_classes = [ExportForest]
+class ImportForest(bpy.types.Operator, ImportHelper):
+    """Import to FOREST file format (.FOREST4.JSON)"""
+    bl_idname = "import_scene.forest"
+    bl_label = 'Import Forest v4'
+
+    filename_ext = ".forest4.json"
+    filter_glob: StringProperty(
+            default="*.forest4.json",
+            options={'HIDDEN'},
+            )
+
+    def draw(self, context):
+        layout = self.layout
+
+    def execute(self, context):
+
+        keywords = self.as_keywords(ignore=("axis_forward",
+                                            "axis_up",
+                                            "filter_glob",
+                                            "check_existing",
+                                            ))
+
+        return import_forest.load(self, context, **keywords)
+
+addon_classes = [ExportForest,
+                ImportForest
+                ]
 
 # Add to a menu
 def menu_func_export(self, context):
     self.layout.operator(ExportForest.bl_idname, text="BeamNG Forest (*.forest4.json)")
 
+def menu_func_import(self, context):
+    self.layout.operator(ImportForest.bl_idname, text="BeamNG Forest (*.forest4.json)")
 
 def register():
     for c in addon_classes:
         bpy.utils.register_class(c)
 
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 
 def unregister():
@@ -91,6 +122,7 @@ def unregister():
         bpy.utils.unregister_class(c)
 
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
 
 if __name__ == "__main__":
     register()
---
+++ io_scene_forest/import_forest.py
@@ -0,0 +1,61 @@
+# ##### BEGIN LICENSE BLOCK #####
+#
+# This program is licensed under The MIT License:
+# see LICENSE for the full license text
+#
+# ##### END LICENSE BLOCK #####
+
+import bpy
+import math
+import mathutils
+import os
+import json
+
+def load(operator, context, filepath):
+    filename = os.path.basename(filepath)
+    forest_item_name = filename.replace(".forest4.json","")
+    if forest_item_name in bpy.data.collections:
+        operator.report({'ERROR'},"Collections already exists")
+        return {"CANCELLED"}
+
+    bpy.ops.collection.create(name=forest_item_name)
+    #add the collection create above to the scene tree
+    context.scene.collection.children.link(bpy.data.collections[forest_item_name])
+
+    #https://devtalk.blender.org/t/set-active-collection/2409/4
+    context.view_layer.active_layer_collection = context.view_layer.layer_collection.children[-1]
+
+    with open(filepath,"r") as f:
+        for line in f:
+            j = json.loads(line)
+            eul = mathutils.Euler((0.0, 0.0, 0.0), 'XYZ')
+            mat = mathutils.Matrix(((1.0, 0.0, 0.0),
+                                    (0.0, 1.0, 0.0),
+                                    (0.0, 0.0, 1.0),
+                                    ))
+            if "rotationMatrix" in j:
+                rotm = j["rotationMatrix"]
+                mat = mathutils.Matrix(((rotm[0],rotm[1],rotm[2]),
+                                        (rotm[3],rotm[4],rotm[5]),
+                                        (rotm[6],rotm[7],rotm[8])))
+                eul = mat.to_euler()
+            elif "quat" in j:
+                q = j["quat"]
+                eul = mathutils.Quaternion((q[0], q[1], q[2], q[3])).to_euler()
+                eul.rotate_axis('Z', math.radians(180))
+            else:
+                operator.report({'ERROR'},"rotation infromation missing")
+                return {"CANCELLED"}
+
+            scale = 1
+            if "scale" in j:
+                scale = j["scale"]
+
+            bpy.ops.object.empty_add(type='ARROWS',
+                radius=scale,
+                align='WORLD',
+                location=(j["pos"][0], j["pos"][1], j["pos"][2]),
+                rotation=(eul.x, eul.y, eul.z))
+            #bpy.ops.object.collection_link(collection=forest_item_name) #not needed as it's auto linked to avtive collection
+
+    return {'FINISHED'}
